65a433c8fd2e58a503176cf79b01f058723c24fb
petkopetkov90 <petkopetkov900808@gmail.com>
2024-10-26 21:24:09 +0300

    Initial commit
    Set up ASP.NET MVC project with layered architecture

---
+++ FleetRouteManager/FleetRouteManager.Data/Data/FleetRouteManagerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetRouteManager.Data.Data
+{
+    public class FleetRouteManagerDbContext : IdentityDbContext<IdentityUser>
+    {
+        public FleetRouteManagerDbContext(DbContextOptions<FleetRouteManagerDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
